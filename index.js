7184b97bb4e03ced8022fcced6ecff052279b840
mubarak-muskanfathima <muskanmubarak73749@gmail.com>
2026-05-02 16:59:43 +0530

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port=process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
